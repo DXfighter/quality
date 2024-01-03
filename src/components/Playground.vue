8c9bfcf5bf4b9f7222e17f90bb0c0d7031cab274
DXfighter <nikito120@abv.bg>
2024-01-03 15:05:50 +0200

    сс

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -2,55 +2,30 @@
   <div class="inline-block space-y-2 relative">
     <!-- Selected Quality -->
     <div
-      :class="[
-        'flex',
-        'items-center',
-        'justify-center',
-        'cursor-pointer',
-        'rounded-full',
-        { hidden: !selectedQuality },
-        'p-3',
-        'w-28',
-        {
-          'bg-green-200': selectedQuality && selectedQuality.color === 'green',
-          'bg-yellow-200':
-            selectedQuality && selectedQuality.color === 'yellow',
-          'bg-red-200': selectedQuality && selectedQuality.color === 'red',
-        },
-      ]"
-      @click="openQuality"
+      :class="['flex items-center justify-center cursor-pointer rounded-full p-3 w-28', {
+        'hidden': !selectedQuality,
+        'bg-green-200': selectedQuality && selectedQuality.color === 'green',
+        'bg-yellow-200': selectedQuality && selectedQuality.color === 'yellow',
+        'bg-red-200': selectedQuality && selectedQuality.color === 'red',
+      }]"
+      @click="toggleQualityOptions"
     >
-      <p
-        :class="[
-          'font-chips-semibold-bolder',
-          selectedQuality ? `text-${selectedQuality.color}-800` : '',
-        ]"
-      >
-        {{ selectedQuality.label }}
+      <p :class="['font-chips-semibold-bolder', selectedQuality ? `text-${selectedQuality.color}-800` : '']">
+        {{ selectedQuality ? selectedQuality.label : '' }}
       </p>
     </div>
 
     <!-- Option -->
-    <div class="absolute">
+    <div v-if="isQualityShown" class="absolute">
       <div
-        v-if="isQualityShown"
         v-for="option in qualityOptions"
         :key="option.value"
-        :class="[
-          'flex',
-          'items-center',
-          'justify-center',
-          { hidden: option.label === selectedQuality.label },
-          'cursor-pointer',
-          'rounded-full',
-          'p-3',
-          'w-28',
-          {
-            'bg-green-200': option.color === 'green',
-            'bg-yellow-200': option.color === 'yellow',
-            'bg-red-200': option.color === 'red',
-          },
-        ]"
+        :class="['flex items-center justify-center cursor-pointer rounded-full p-3 w-28', {
+          'hidden': option.label === selectedQuality.label,
+          'bg-green-200': option.color === 'green',
+          'bg-yellow-200': option.color === 'yellow',
+          'bg-red-200': option.color === 'red',
+        }]"
         @click="selectQuality(option) && closeQuality"
       >
         <p :class="['font-chips-semibold-bolder', `text-${option.color}-800`]">
@@ -58,6 +33,7 @@
         </p>
       </div>
     </div>
+
     {{ qualityInfo }}
   </div>
 </template>
@@ -84,10 +60,10 @@ export default defineComponent({
   methods: {
     selectQuality(option) {
       this.selectedQuality = option;
+      this.$emit('update:qualityInfo', option.label); // Emit selected quality label
       this.closeQuality();
-      this.$emit('update:qualityInfo', option.label);
     },
-    openQuality() {
+    toggleQualityOptions() {
       this.isQualityShown = !this.isQualityShown;
     },
     closeQuality() {
@@ -98,9 +74,7 @@ export default defineComponent({
     qualityInfo: {
       immediate: true,
       handler(newValue) {
-        const selected = this.qualityOptions.find(
-          (option) => option.label === newValue
-        );
+        const selected = this.qualityOptions.find(option => option.label === newValue);
         if (selected) {
           this.selectedQuality = selected;
         }
